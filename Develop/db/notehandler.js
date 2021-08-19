61094aff478a4ae7fb264f82847fd1338cdcd316
catupperman <catupperman@gmail.com>
2021-08-19 13:06:31 -0500

    giving up

--- Develop/db/notehandler.js
+++ Develop/db/notehandler.js
@@ -27,19 +27,19 @@ class NoteHandler {
     write(note) {
         return writeFile("db/db.json", JSON.stringify(note))
     }
-    //attempting to take the written notes and the notes already within the db.json and save to the db.json, so they appear on the screen when saved (this is a failed function, that I have been spending 30 plus hours trying to fix)
-    createNotes(note) {
-       const { title, text } = note;
-       const noteObj = { title, text, id: uuidv4() };
-       const notes = this.readnotes();
-       console.log(notes);
-       const newNotesArr = [...notes, noteObj];
-       console.log(newNotesArr);
-       this.write(newNotesArr);
-       return "a";
-   }
+    //attempting to take the written notes and the notes already within the db.json and save to the db.json, so they appear on the screen when saved (this is a failed function, that I have been spending 30 plus hours trying to fix)  Insomnia indicates note is undefined when taking away the synatx for an async function.  I attempted to get help from my tutor on this, but they could not figure out why note was readable within the write portion of this constructor function, but does not exist within the createnotes portion of this constructor function. 
+    async createNotes(note) {
+        const { title, text } = note;
+        const noteObj = { title, text, id: uuidv4() };
+        const notes = await this.readnotes();
+        console.log(notes);
+        const newNotesArr = [...notes, noteObj];
+        console.log(newNotesArr);
+        await this.write(newNotesArr);
+        return "a";
+    }
 }
 
 
-//exports the new notes
+//exports the notes to the html through the api.js file
 module.exports = new NoteHandler()
